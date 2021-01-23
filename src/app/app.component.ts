740f4cb8d0fd2dfcd55ffdc0f6d10405fcfc592d
Ryo <endo.ryo.mm@gmail.com>
2021-01-23 09:30:46 +0900

    メッセージの編集と削除の実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import {　Comment　} from './class/comment';
 import { User } from './class/user';
@@ -8,31 +10,47 @@ import { User } from './class/user';
 const CURRENT_USER: User = new User(1, '遠藤　瞭');
 const ANOTHER_USER: User = new User(2,'テストユーザ');
 
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です'),
-  new Comment(ANOTHER_USER, 'テストメッセージ'),
-  new Comment(CURRENT_USER, 'お疲れ様です'),
-  new Comment(CURRENT_USER, '了解しました'),
-]
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  comments = COMMENTS;
+  
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = "";
-  item$ : Observable<any>;
+  
 
   constructor( private db: AngularFireDatabase) {
-    this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+
+    //firevbaseのRealtimeDatabaseに格納されている「comment」のメタ情報取得,コメントの編集を行うため
+    this.comments$ = this.commentsRef.snapshotChanges()
+      .pipe (
+        map((snapshots: SnapshotAction<Comment>[]) => {
+          return snapshots.map(snapshot => {
+            const value = snapshot.payload.val();
+            return new Comment({key: snapshot.payload.key, ...value });
+          })
+        })
+      )
   }
 
-  addComment(comment: string) :void{
+  addComment(comment: string) :void {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment({user: this.currentUser, message: comment}));
+      this.comment = '';
     }
+  }
+
+  updateComment(comment: Comment) :void {
+    const { key, message } = comment;
+    this.commentsRef.update(key!, { message });
+  }
+
+  deleteComment(comment: Comment) :void {
+    this.commentsRef.remove(comment.key);
   }
 }
